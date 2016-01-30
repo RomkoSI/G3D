6013a992c8114a5aeb3d67502345925acd78519b
Mx7f <Mx7f@9296df4a-2249-4d1b-8fb0-1ab621adc933>
2016-01-30 05:12:10 +0000

    Provisional implementation of FileSystem::copyDirectory()

--- scratch/michael/source/App.h
+++ scratch/michael/source/App.h
@@ -1,22 +1,42 @@
+/**
+\file App.h
+
+The G3D 10.00 default starter app is configured for OpenGL 3.3 and
+relatively recent GPUs.
+*/
 #ifndef App_h
 #define App_h
-
 #include <G3D/G3DAll.h>
 
+/** Application framework. */
 class App : public GApp {
-    shared_ptr<Renderer> m_otherRenderer;
 protected:
-    void makeGUI();
 
-    void computeGBuffer(RenderDevice* rd, Array<shared_ptr<Surface> >& all);
-    void computeShadows(RenderDevice* rd, Array<shared_ptr<Surface> >& all, LightingEnvironment& environment);
-    void deferredShade(RenderDevice* rd, const LightingEnvironment& environment);
-    void forwardShade(RenderDevice* rd, Array<shared_ptr<Surface> >& all, const LightingEnvironment& environment);
+	/** Called from onInit */
+	void makeGUI();
 
 public:
 
-    App(const GApp::Settings& settings = GApp::Settings());
-    virtual void onInit() override;
+	App(const GApp::Settings& settings = GApp::Settings());
+
+	virtual void onInit() override;
+	virtual void onAI() override;
+	virtual void onNetwork() override;
+	virtual void onSimulation(RealTime rdt, SimTime sdt, SimTime idt) override;
+	virtual void onPose(Array<shared_ptr<Surface> >& posed3D, Array<shared_ptr<Surface2D> >& posed2D) override;
+
+	// You can override onGraphics if you want more control over the rendering loop.
+	// virtual void onGraphics(RenderDevice* rd, Array<shared_ptr<Surface> >& surface, Array<shared_ptr<Surface2D> >& surface2D) override;
+
+	virtual void onGraphics3D(RenderDevice* rd, Array<shared_ptr<Surface> >& surface3D) override;
+	virtual void onGraphics2D(RenderDevice* rd, Array<shared_ptr<Surface2D> >& surface2D) override;
+
+	virtual bool onEvent(const GEvent& e) override;
+	virtual void onUserInput(UserInput* ui) override;
+	virtual void onCleanup() override;
+
+	/** Sets m_endProgram to true. */
+	virtual void endProgram();
 };
 
 #endif
