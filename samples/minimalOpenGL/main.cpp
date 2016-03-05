eb38b94b2c360a92d23cdf3c1f1f46d9e66ce75e
morgan <morgan@9296df4a-2249-4d1b-8fb0-1ab621adc933>
2016-03-05 20:39:48 +0000

    Added cube mesh

--- samples/minimalOpenGL/main.cpp
+++ samples/minimalOpenGL/main.cpp
@@ -30,101 +30,48 @@
   TODO:
     - Draw a cube + fake shadow
     - Show how to load a texture map
-    - Matrix4x4 inverse
     - Matrix3x3
     - Add key bindings  
 */
 
 #include "minimalOpenGL.h"
-#include <iostream>
 
 GLFWwindow* window = nullptr;
 
-
 int main(const int argc, const char* argv[]) {
-    window = initOpenGL(1280, 720, "minimalOpenGL");
+    const int windowWidth = 1280, windowHeight = 720;
+    window = initOpenGL(windowWidth, windowHeight, "minimalOpenGL");
 
-    const Geometry& geom = Geometry::cube();
-
-    const Vector3 cpuPosition[] = {
-        /*
-        // Right triangle in pixel coords:
-        Vector3(0, 0, 0),
-        Vector3(1280, 0, 0),
-        Vector3(0, 720, 0)
-        */
-
-        // Isosceles triangle:
-        Vector3( 0.0f, 2.0f, 0.0f),
-        Vector3(-1.0f, 0.0f, 0.0f),
-        Vector3( 1.0f, 0.0f, 0.0f)
-       
-
-        // Right triangle:
-        /*
-        Vector3( 0.0f,  0.0f, 0.0f),
-        Vector3( 1.0f,  0.0f, 0.0f),
-        Vector3( 0.0f,  1.0f, 0.0f)
-        */
-    };
-
-    const Vector3 cpuColor[] = {
-        Vector3( 1.0f, 0.0f, 0.0f),
-        Vector3( 0.0f, 1.0f, 0.0f),
-        Vector3( 0.0f, 0.0f, 1.0f),
-    };
-
-
-    // Create separate vertex buffers for each attribute
+    /////////////////////////////////////////////////////////////////
+    // Load vertex array buffers
     const int N = 3;
     GLuint positionBuffer = GL_NONE;
     glGenBuffers(1, &positionBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, positionBuffer);
-    glBufferData(GL_ARRAY_BUFFER, N * sizeof(Vector3), cpuPosition, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Cube::position), Cube::position, GL_STATIC_DRAW);
 
-    GLuint colorBuffer = GL_NONE;
-    glGenBuffers(1, &colorBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
-    glBufferData(GL_ARRAY_BUFFER, N * sizeof(Vector3), cpuColor, GL_STATIC_DRAW);
+    GLuint normalBuffer = GL_NONE;
+    glGenBuffers(1, &normalBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, normalBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Cube::normal), Cube::normal, GL_STATIC_DRAW);
 
+    GLuint indexBuffer = GL_NONE;
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Cube::index), Cube::index, GL_STATIC_DRAW);
+
+    /////////////////////////////////////////////////////////////////////
+    // Create the main shader
     const GLuint shader = loadShaderProgram("min.vrt", "min.pix");
 
     // Binding points for attributes and uniforms discovered from the shader
     const GLint positionAttribute                = glGetAttribLocation(shader,  "position");
-    const GLint colorAttribute                   = glGetAttribLocation(shader,  "color");
+    const GLint normalAttribute                  = glGetAttribLocation(shader,  "normal");
     const GLint modelViewProjectionMatrixUniform = glGetUniformLocation(shader, "modelViewProjectionMatrix");
-
-    int windowWidth = 1, windowHeight = 1;
-    glfwGetWindowSize(window, &windowWidth, &windowHeight);
-
-    // Init framebuffer and targets
-    GLuint framebuffer = GL_NONE;
-    {
-        glGenFramebuffers(1, &framebuffer);
-        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-        
-        GLuint colorBuffer = GL_NONE;
-        glGenTextures(1, &colorBuffer);
-        glBindTexture(GL_TEXTURE_2D, colorBuffer);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, windowWidth, windowHeight, 0, GL_RGBA16F, GL_FLOAT, nullptr);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorBuffer, 0);
-        
-        GLuint depthBuffer = GL_NONE;
-        glGenTextures(1, &depthBuffer);
-        glBindTexture(GL_TEXTURE_2D, depthBuffer);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, windowWidth, windowHeight, 0, GL_R32F, GL_FLOAT, nullptr);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthBuffer, 0);
-        
-        // Restore the hardware framebuffer
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
-    }
 
     // Main loop:
     int timer = 0;
     while (! glfwWindowShouldClose(window)) {
-        // Render to the off-screen HDR framebuffer
-        // glBindFramebuffer(GL_DRAW_FRAMEBUFFER, framebuffer);
-
         const float nearPlaneZ = -0.1f;
         const float farPlaneZ = -100.0f;
         const float verticalFieldOfView = 45.0f * PI / 180.0f;
@@ -132,13 +79,15 @@ int main(const int argc, const char* argv[]) {
         glClearColor(0.1f, 0.2f, 0.3f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        const Matrix4x4 objectToWorldMatrix;
+        const Matrix4x4& objectToWorldMatrix = Matrix4x4::translate(0.0f, 0.5f, 0.0f) * Matrix4x4::yaw(PI / 4.0f);
         const Matrix4x4& cameraToWorldMatrix = Matrix4x4::translate(0.0, 1.0, 3.0f);// *Matrix4x4::yaw(timer * 0.001f);
         const Matrix4x4& projectionMatrix = Matrix4x4::perspective(float(windowWidth), float(windowHeight), nearPlaneZ, farPlaneZ, verticalFieldOfView);
 
         // Draw the background
         drawSky(windowWidth, windowHeight, nearPlaneZ, farPlaneZ, verticalFieldOfView, cameraToWorldMatrix);
 
+        ////////////////////////////////////////////////////////////////////////
+        // Draw a mesh
         glEnable(GL_DEPTH_TEST);
         glDepthFunc(GL_LESS);
         glDisable(GL_CULL_FACE);
@@ -153,24 +102,18 @@ int main(const int argc, const char* argv[]) {
         glVertexAttribPointer(positionAttribute, N, GL_FLOAT, GL_FALSE, 0, 0);
         glEnableVertexAttribArray(positionAttribute);
 
-        glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
-        glVertexAttribPointer(colorAttribute, N, GL_FLOAT, GL_FALSE, 0, 0);
-        glEnableVertexAttribArray(colorAttribute);
+        glBindBuffer(GL_ARRAY_BUFFER, normalBuffer);
+        glVertexAttribPointer(normalAttribute, N, GL_FLOAT, GL_FALSE, 0, 0);
+        glEnableVertexAttribArray(normalAttribute);
 
-        glDrawArrays(GL_TRIANGLES, 0, N);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+        glDrawElements(GL_TRIANGLES, sizeof(Cube::index) / sizeof(Cube::index[0]), GL_UNSIGNED_INT, 0);
+
+        ////////////////////////////////////////////////////////////////////////
 
         // Check for events
         glfwPollEvents();
-
-        /*
-        // Blit the HDR framebuffer to the LDR hardware framebuffer
-        glBindFramebuffer(GL_READ_FRAMEBUFFER, framebuffer);
-        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
-        glBlitFramebuffer(0, 0, windowWidth, windowHeight, 
-                          0, 0, windowWidth, windowHeight, 
-                          GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT,
-                          GL_NEAREST);
-        */
 
         // Display what has been drawn
         glfwSwapBuffers(window);
@@ -193,4 +136,42 @@ int main(const int argc, const char* argv[]) {
     int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR szCmdLine, int sw) {
         return main(0, nullptr);
     }
+#endif
+
+
+
+#if 0
+
+    // Init framebuffer and targets
+    GLuint framebuffer = GL_NONE;
+    {
+        glGenFramebuffers(1, &framebuffer);
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+        GLuint colorBuffer = GL_NONE;
+        glGenTextures(1, &colorBuffer);
+        glBindTexture(GL_TEXTURE_2D, colorBuffer);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, windowWidth, windowHeight, 0, GL_RGBA16F, GL_FLOAT, nullptr);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorBuffer, 0);
+
+        GLuint depthBuffer = GL_NONE;
+        glGenTextures(1, &depthBuffer);
+        glBindTexture(GL_TEXTURE_2D, depthBuffer);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, windowWidth, windowHeight, 0, GL_R32F, GL_FLOAT, nullptr);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthBuffer, 0);
+
+        // Restore the hardware framebuffer
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    }
+
+
+    /*
+    // Blit the HDR framebuffer to the LDR hardware framebuffer
+    glBindFramebuffer(GL_READ_FRAMEBUFFER, framebuffer);
+    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+    glBlitFramebuffer(0, 0, windowWidth, windowHeight,
+    0, 0, windowWidth, windowHeight,
+    GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT,
+    GL_NEAREST);
+    */
 #endif
