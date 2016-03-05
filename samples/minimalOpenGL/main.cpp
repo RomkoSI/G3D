7b425fb525b534a4d54cb48ce080ea6ae8b04e77
morgan <morgan@9296df4a-2249-4d1b-8fb0-1ab621adc933>
2016-03-05 13:18:35 +0000

    Added background and error checking

--- samples/minimalOpenGL/main.cpp
+++ samples/minimalOpenGL/main.cpp
@@ -39,22 +39,9 @@
 
 GLFWwindow* window = nullptr;
 
-void APIENTRY debugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
-    if ((type == GL_DEBUG_TYPE_ERROR) || (type == GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR)) {
-        fprintf(stderr, "GL Debug: %s\n", message);
-    }
-}
-
 
 int main(const int argc, const char* argv[]) {
     window = initOpenGL(1280, 720, "minGL4");
-
-#   ifdef _DEBUG
-       glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-       glEnable(GL_DEBUG_OUTPUT);
-       // Causes a segmentation fault on OS X
-       glDebugMessageCallback(debugCallback, nullptr);
-#   endif
 
     const Vector3 cpuPosition[] = {
         /*
@@ -199,3 +186,10 @@ int main(const int argc, const char* argv[]) {
     glfwTerminate();
     return 0;
 }
+
+
+#ifdef G3D_WINDOWS
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR szCmdLine, int sw) {
+    return main(0, nullptr);
+}
+#endif
