f5b730d61d583119eaa123aaba001407ce4a0a43
morgan <morgan@9296df4a-2249-4d1b-8fb0-1ab621adc933>
2016-03-06 00:39:05 +0000

    Texture mapping

--- G3D10/samples/minimalOpenGL/minimalOpenGL.h
+++ G3D10/samples/minimalOpenGL/minimalOpenGL.h
@@ -426,6 +426,155 @@ public:
 };
 
 
+
+class Matrix3x3 {
+public:
+    /** row-major */
+    float data[9];
+
+    /** row-major */
+    Matrix3x3(float a, float b, float c, 
+        float d, float e, float f, 
+        float g, float h, float i) {
+        data[0] = a; data[1] = b; data[2] = c; 
+        data[3] = d; data[4] = e; data[5] = f; 
+        data[6] = g; data[7] = h; data[8] = i;
+    }
+
+    /** Takes the upper-left 3x3 submatrix */
+    Matrix3x3(const Matrix4x4& M) {
+        data[0] = M.data[0]; data[1] = M.data[1]; data[2] = M.data[2];
+        data[3] = M.data[4]; data[4] = M.data[5]; data[5] = M.data[6];
+        data[6] = M.data[8]; data[7] = M.data[9]; data[8] = M.data[10];
+    }
+
+    /** initializes to the identity matrix */
+    Matrix3x3() {
+        memset(data, 0, sizeof(float) * 9);
+        data[0] = data[4] = data[8] = 1.0f;
+    }
+
+    Matrix3x3(const Matrix3x3& M) {
+        memcpy(data, M.data, sizeof(float) * 9);
+    }
+
+    Matrix3x3& operator=(const Matrix3x3& M) {
+        memcpy(data, M.data, sizeof(float) * 9);
+        return *this;
+    }
+
+    static Matrix3x3 zero() {
+        return Matrix3x3(0.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.0f);
+    }
+
+    static Matrix3x3 roll(float radians) {
+        const float c = cos(radians), s = sin(radians);
+        return Matrix3x3(c, -s, 0.0f,
+            s, c, 0.0f,
+            0.0f, 0.0f, 1.0f);
+    }
+
+    static Matrix3x3 yaw(float radians) {
+        const float c = cos(radians), s = sin(radians);
+        return Matrix3x3(c, 0.0f, s,
+            0.0f, 1.0f, 0.0f,
+            -s, 0.0f, c);
+    }
+
+    static Matrix3x3 pitch(float radians) {
+        const float c = cos(radians), s = sin(radians);
+        return Matrix3x3(1.0f, 0.0f, 0.0f,
+            0.0f, c, -s,
+            0.0f, s, c);
+    }
+
+    static Matrix3x3 scale(float x, float y, float z) {
+        return Matrix3x3(x, 0.0f, 0.0f,
+            0.0f, y, 0.0f,
+            0.0f, 0.0f, z);
+    }
+
+    Matrix3x3 transpose() const {
+        return Matrix3x3(data[0], data[3], data[6],
+            data[1], data[4], data[7],
+            data[2], data[5], data[8]);
+    }
+
+    /** Computes the inverse by Cramer's rule */
+    Matrix3x3 inverse() const {
+        const Matrix3x3& m(*this);
+        const float det = m(0, 0) * (m(1, 1) * m(2, 2) - m(2, 1) * m(1, 2)) -
+                          m(0, 1) * (m(1, 0) * m(2, 2) - m(1, 2) * m(2, 0)) +
+                          m(0, 2) * (m(1, 0) * m(2, 1) - m(1, 1) * m(2, 0));
+
+        return Matrix3x3(
+            (m(1, 1) * m(2, 2) - m(2, 1) * m(1, 2)) / det,
+            (m(0, 2) * m(2, 1) - m(0, 1) * m(2, 2)) / det,
+            (m(0, 1) * m(1, 2) - m(0, 2) * m(1, 1)) / det,
+
+            (m(1, 2) * m(2, 0) - m(1, 0) * m(2, 2)) / det,
+            (m(0, 0) * m(2, 2) - m(0, 2) * m(2, 0)) / det,
+            (m(1, 0) * m(0, 2) - m(0, 0) * m(1, 2)) / det,
+
+            (m(1, 0) * m(2, 1) - m(2, 0) * m(1, 1)) / det,
+            (m(2, 0) * m(0, 1) - m(0, 0) * m(2, 1)) / det,
+            (m(0, 0) * m(1, 1) - m(1, 0) * m(0, 1)) / det);
+    }
+
+    float& operator()(int r, int c) {
+        return data[r * 3 + c];
+    }
+
+    float operator()(int r, int c) const {
+        return data[r * 3 + c];
+    }
+
+    Vector3 row(int r) const {
+        const int i = r * 3;
+        return Vector3(data[i], data[i + 1], data[i + 2]);
+    }
+
+    Vector3 col(int c) const {
+        return Vector3(data[c], data[c + 3], data[c + 6]);
+    }
+
+    Matrix3x3 operator*(const Matrix3x3& B) const {
+        Matrix3x3 D;
+        for (int r = 0; r < 3; ++r) {
+            for (int c = 0; c < 3; ++c) {
+                D(r, c) = row(r).dot(B.col(c));
+            }
+        }
+        return D;
+    }
+
+    Matrix3x3 operator*(const float s) const {
+        Matrix3x3 D;
+        for (int i = 0; i < 9; ++i) {
+            D.data[i] = data[i] * s;
+        }
+        return D;
+    }
+
+    Matrix3x3 operator/(const float s) const {
+        Matrix3x3 D;
+        for (int i = 0; i < 9; ++i) {
+            D.data[i] = data[i] / s;
+        }
+        return D;
+    }
+
+    Vector3 operator*(const Vector3& v) const {
+        Vector3 d;
+        for (int r = 0; r < 3; ++r) {
+            d[r] = row(r).dot(v);
+        }
+        return d;
+    }
+};
+
 std::ostream& operator<<(std::ostream& os, const Vector3& v) {
     return os << "Vector3(" << std::setprecision(2) << v.x << ", " << v.y << ")";
 }
@@ -445,6 +594,21 @@ std::ostream& operator<<(std::ostream& os, const Matrix4x4& M) {
             if (c < 3) { os << ", "; }
         }
         if (r < 3) { os << ",\n          "; }
+    }
+
+    return os << ")\n";
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Matrix3x3& M) {
+    os << "\nMatrix3x3(";
+
+    for (int r = 0, i = 0; r < 3; ++r) {
+        for (int c = 0; c < 3; ++c, ++i) {
+            os << M.data[i];
+            if (c < 2) { os << ", "; }
+        }
+        if (r < 2) { os << ",\n          "; }
     }
 
     return os << ")\n";
