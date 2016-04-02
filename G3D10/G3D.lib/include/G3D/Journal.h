4a64b9160dc3e261602686c6466d8abfa12bc5dd
morgan <morgan@9296df4a-2249-4d1b-8fb0-1ab621adc933>
2016-04-02 01:42:59 +0000

    abstracted screenshot dialog Doxygen syntax

--- G3D10/G3D.lib/include/G3D/Journal.h
+++ G3D10/G3D.lib/include/G3D/Journal.h
@@ -4,10 +4,10 @@
   \maintainer Morgan McGuire, http://graphics.cs.williams.edu
 
   \created 2014-07-17
-  \edited  2014-07-17
+  \edited  2016-04-01
 
  G3D Innovation Engine
- Copyright 2000-2015, Morgan McGuire.
+ Copyright 2000-2016, Morgan McGuire.
  All rights reserved.
  */
 #ifndef G3D_Journal_h
@@ -24,25 +24,34 @@ namespace G3D {
   */
 class Journal {
 private:
+
     // Currently an abstract class
     Journal() {}
 
 public:
 
-    /** Locates journal.dox and returns the fully-qualified filename, starting a search from \a hint. 
-        Returns the empty string if not found.
-     */
+    /** Locates journal.dox or journal.md.html and returns the
+        fully-qualified filename, starting a search from \a hint.
+        Returns the empty string if not found. */
     static String findJournalFile(const String& hint = ".");
 
-    /** Returns title of the first Doxygen "section" command the journal at \a journalFilename, or the empty
-        string if no section title is found. Assumes that \a journalFilename exists. */
+    /** Returns title of the first Doxygen "section" or Markdeep level
+        1 header (underlined by === or prefixed by a single #) command
+        the journal at \a journalFilename, or the empty string if no
+        section title is found. Assumes that \a journalFilename
+        exists. */
     static String firstSectionTitle(const String& journalFilename);
 
-    /** Adds \a text to the bottom of the first section in the .dox file at \a journalFilename. */
+    /** Adds \a text to the bottom of the first section in the .dox or .md.html
+        file at \a journalFilename. */
     static void appendToFirstSection(const String& journalFilename, const String& text);
 
-    /** Inserts \a text immediately before the first "section" command in the .dox file at \a journalFilename. */
-    static void insertBeforeFirstSection(const String& journalFilename, const String& text);
+    /** Inserts \a text immediately before the first "section" command
+        in the .dox file or before the first level 1 header in the
+        .md.html file at \a journalFilename. */
+    static void insertNewSection(const String& journalFilename, const String& title, const String& text);
+
+    static String formatImage(const String& journalFilename, const String& imageFilename, const String& caption, const String& discussion);
 };
 
 }
