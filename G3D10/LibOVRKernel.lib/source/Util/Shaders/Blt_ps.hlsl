6eb962bb6303de0c6ba2ca81334e20ed04ea6e25
morgan <morgan@9296df4a-2249-4d1b-8fb0-1ab621adc933>
2016-05-24 23:13:56 +0000

    removed oculus library

--- G3D10/LibOVRKernel.lib/source/Util/Shaders/Blt_ps.hlsl
+++
@@ -1,8 +0,0 @@
-Texture2D Input;
-sampler Sampler;
-
-float4 main(float4 pos : SV_POSITION,
-            float2 tex : TEXCOORD) : SV_TARGET
-{
-    return Input.Sample(Sampler, tex);
-}
--- G3D10/LibOVRKernel.lib/source/Util/Shaders/Blt_vs.hlsl
+++
@@ -1,8 +0,0 @@
-void main(float2 pos : POSITION,
-          float2 tex : TEXCOORD,
-          out float4 oPos : SV_POSITION,
-          out float2 oTex : TEXCOORD)
-{
-    oPos = float4(pos, 0, 1);
-    oTex = tex;
-}
