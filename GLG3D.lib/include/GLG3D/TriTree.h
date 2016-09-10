08262ad1d8c8c140fc79493fb7a08bc840af58a8
morgan <morgan@9296df4a-2249-4d1b-8fb0-1ab621adc933>
2016-09-10 03:35:13 +0000

    Refactored TriTreeBase into its own header/cpp files from TriTree

--- GLG3D.lib/include/GLG3D/TriTree.h
+++ GLG3D.lib/include/GLG3D/TriTree.h
@@ -11,6 +11,7 @@
 
 #include <functional>
 #include "G3D/platform.h"
+#include "GLG3D/TriTreeBase.h"
 #include "G3D/Vector3.h"
 #include "G3D/Color3.h"
 #include "G3D/AABox.h"
@@ -28,136 +29,9 @@
 #endif
 
 namespace G3D {
-class Surface;
-class Surfel;
-class Material;
-
-/** Base class for ray-casting data structures. */
-class TriTreeBase : public ReferenceCountedObject {
-protected:
-
-    Array<Tri>              m_triArray;
-    CPUVertexArray          m_vertexArray;
-
-public:
-
-    /** CPU timing of API conversion overhead for the most recent call to intersectRays */
-    mutable RealTime debugConversionOverheadTime = 0;
-
-    /** Options for intersectRays. Default is full intersection with no backface culling optimization and partial coverage (alpha) test passing for values over 0.5. */
-    typedef unsigned int IntersectRayOptions;
-
-    /** Test for occlusion and do not necessarily return valid triIndex, backfacing, etc. data
-        (useful for shadow rays and testing line of sight) */
-    static const IntersectRayOptions OCCLUSION_TEST_ONLY = 1;
-
-    /** Do not allow the intersector to perform backface culling as an
-        optimization. Backface culling is not required in any case. */
-    static const IntersectRayOptions DO_NOT_CULL_BACKFACES = 2;
-
-    /** Only fail the partial coverage (alpha) test on zero coverage. */
-    static const IntersectRayOptions PARTIAL_COVERAGE_THRESHOLD_ZERO = 4;
-
-    /** Disable partial coverage (alpha) testing. */
-    static const IntersectRayOptions NO_PARTIAL_COVERAGE_TEST = 8;
-
-    /** Make optimizations appropriate for coherent rays (same origin) */
-    static const IntersectRayOptions COHERENT_RAY_HINT = 16;
-    
-    class Hit {
-    public:
-        enum { NONE = -1 };
-        /** NONE if no hit. For occlusion ray casts, this will be an undefined value not equal to NONE. */
-        int         triIndex;
-        float       u;
-        float       v;
-        float       distance;
-
-        /** For occlusion ray casts, this will always be false. */
-        bool        backface;
-
-        Hit() : triIndex(NONE), u(0), v(0), distance(0), backface(false) {}
-    };
-
-    virtual ~TriTreeBase();
-
-    virtual void clear();
-
-    const Array<Tri>& triArray() const {
-        return m_triArray;
-    }
-
-    const CPUVertexArray& vertexArray() const {
-        return m_vertexArray;
-    }
-
-    /** Array access to the stored Tris */
-    const Tri& operator[](int i) const {
-        debugAssert(i >= 0 && i < m_triArray.size());
-        return m_triArray[i];
-    }
-
-    int size() const {
-        return m_triArray.size();
-    }
-
-    /** Base class implementation populates m_triArray and m_vertexArray and applies the image storage option. */
-    virtual void setContents
-        (const Array<shared_ptr<Surface>>&  surfaceArray, 
-         ImageStorage                       newImageStorage = ImageStorage::COPY_TO_CPU);
-
-    virtual void setContents
-       (const Array<Tri>&                   triArray, 
-        const CPUVertexArray&               vertexArray,
-        ImageStorage                        newStorage = ImageStorage::COPY_TO_CPU);
-
-    /** Helper function that samples materials. The default implementation calls the intersectRay
-        override that takes a Hit and then samples from it.
-
-        \param directiondX Reserved for future use
-        \param directiondY Reserved for future use
-     */
-    virtual shared_ptr<Surfel> intersectRay
-        (const Ray&                         ray, 
-         IntersectRayOptions                options         = IntersectRayOptions(0),
-         const Vector3&                     directiondX     = Vector3::zero(),
-         const Vector3&                     directiondY     = Vector3::zero()) const;
-
-    /** Intersect a single ray. Return value is `hit.triIndex != Hit::NONE` for convenience. 
-        \param filterFunction Set to nullptr to accept any geometric ray-triangle instersection.
-      */
-    virtual bool intersectRay
-        (const Ray&                         ray,
-         Hit&                               hit,
-         IntersectRayOptions                options         = IntersectRayOptions(0)) const = 0;
-
-    /** Batch ray casting. The default implementation calls the single-ray version using
-        Thread::runConcurrently. */
-    virtual void intersectRays
-        (const Array<Ray>&                  rays,
-         Array<Hit>&                        results,
-         IntersectRayOptions                options         = IntersectRayOptions(0)) const;
-
-    /** Returns all triangles that lie within the box. Default implementation
-        tests each triangle in turn (linear time). */
-    virtual void intersectBox
-        (const AABox&                       box,
-         Array<Tri>&                        results) const;
-
-    /** Returns all triangles that intersect or are contained within
-        the sphere (technically, this is a ball intersection).
-
-        Default implementation calls intersectBox and then filters the results for the sphere.
-     */
-    virtual void intersectSphere
-        (const Sphere&                      sphere,
-         Array<Tri>&                        triArray) const;
-
-    virtual shared_ptr<Surfel> sample(const Hit& hit) const;
-};
-
 /**
- \brief Static bounding interval hierarchy for Ray-Tri intersections.
+ \brief Native C++ implementation of a static bounding interval hierarchy
+        that is very good for box queries and OK for ray-triangle.
 
  The BIH is a tree in which each node is an axis-aligned box
  containing up to three child nodes: elements in the negative half
--- GLG3D.lib/source/TriTree.cpp
+++ GLG3D.lib/source/TriTree.cpp
@@ -4,7 +4,7 @@
   \maintainer Morgan McGuire, http://graphics.cs.williams.edu
 
   \created 2009-06-10
-  \edited  2016-07-05
+  \edited  2016-09-09
 */
 
 #include "G3D/AreaMemoryManager.h"
@@ -24,125 +24,6 @@ namespace G3D {
 #pragma float_control( except, off )
 #pragma float_control( precise, off )
 #endif
-
-void TriTreeBase::clear() {
-    m_triArray.fastClear();
-    m_vertexArray.clear();
-}
-
-
-TriTreeBase::~TriTreeBase() {
-    clear();
-}
-
-
-void TriTreeBase::setContents
-(const Array<shared_ptr<Surface> >& surfaceArray, 
- ImageStorage                       newStorage) {
-
-    const bool computePrevPosition = false;
-    clear();
-    Surface::getTris(surfaceArray, m_vertexArray, m_triArray, computePrevPosition);
-
-    if (newStorage != IMAGE_STORAGE_CURRENT) {
-        for (int i = 0; i < m_triArray.size(); ++i) {
-            const Tri& tri = m_triArray[i];
-            const shared_ptr<Material>& material = tri.material();
-            if (notNull(material)) {
-                material->setStorage(newStorage);
-            }
-        }
-    }
-}
-
-
-void TriTreeBase::setContents
-   (const Array<Tri>&                  triArray, 
-    const CPUVertexArray&              vertexArray,
-    ImageStorage                       newStorage) {
-
-    //const bool computePrevPosition = false;
-    clear();
-
-    m_triArray = triArray;
-    m_vertexArray.copyFrom(vertexArray);
-   
-    Tri::setStorage(m_triArray, newStorage);
-}
-
-
-void TriTreeBase::intersectRays
-   (const Array<Ray>&      rays,
-    Array<Hit>&            results,
-    IntersectRayOptions    options) const {
-
-    results.resize(rays.size());
-    Thread::runConcurrently(0, rays.size(), [&](int i) { intersectRay(rays[i], results[i], options); });
-}
-
-
-shared_ptr<Surfel> TriTreeBase::intersectRay
-   (const Ray&             ray, 
-    IntersectRayOptions    options,
-    const Vector3&         directiondX,
-    const Vector3&         directiondY) const {
- 
-    Hit hit;
-    if (intersectRay(ray, hit, options)) {
-        return m_triArray[hit.triIndex].sample(hit.u, hit.v, hit.triIndex, m_vertexArray, hit.backface);
-    } else {
-        return nullptr;
-    }
-}
-
-
-void TriTreeBase::intersectBox
-   (const AABox&           box,
-    Array<Tri>&            results) const {
-
-    results.fastClear();
-    Spinlock resultLock;
-    Thread::runConcurrently(0, m_triArray.size(), [&](int t) {
-        const Tri& tri = m_triArray[t];
-        if ((tri.area() > 0.0f) &&
-            CollisionDetection::fixedSolidBoxIntersectsFixedTriangle(box, Triangle(tri.position(m_vertexArray, 0), 
-                                                                                   tri.position(m_vertexArray, 1), 
-                                                                                   tri.position(m_vertexArray, 2)))) {
-            resultLock.lock();
-            results.append(tri);
-            resultLock.unlock();
-        }
-    });
-}
-
-
-void TriTreeBase::intersectSphere
-   (const Sphere&                      sphere,
-    Array<Tri>&                        triArray) const {
-
-    AABox box;
-    sphere.getBounds(box);
-    intersectBox(box, triArray);
-
-    // Iterate backwards because we're removing
-    for (int i = triArray.size() - 1; i >= 0; --i) {
-        const Tri& tri = triArray[i];
-        if (! CollisionDetection::fixedSolidSphereIntersectsFixedTriangle(sphere, Triangle(tri.position(m_vertexArray, 0), tri.position(m_vertexArray, 1), tri.position(m_vertexArray, 2)))) {
-            triArray.fastRemove(i);
-        }
-    }
-}
-
-
-shared_ptr<Surfel> TriTreeBase::sample(const Hit& hit) const {
-    if (hit.triIndex != Hit::NONE) {
-        return m_triArray[hit.triIndex].sample(hit.u, hit.v, hit.triIndex, m_vertexArray, hit.backface);
-    } else {
-        return nullptr;
-    }
-}
-
-/////////////////////////////////////////////
 
 const char* TriTree::algorithmName(SplitAlgorithm s) {
     const char* n[] = {"Mean extent", "Median area", "Median count", "SAH"};
