ebb25ed3b984d2afdf54c1dfc67a170d19a38d4c
morgan <morgan@9296df4a-2249-4d1b-8fb0-1ab621adc933>
2016-10-08 11:24:47 +0000

    Filled in edges

--- G3D10/G3D.lib/source/CubeMap.cpp
+++ G3D10/G3D.lib/source/CubeMap.cpp
@@ -41,23 +41,105 @@ CubeMap::CubeMap
             "Cube maps must use square faces with the same format");
     }
 
+    // Constants for specifying adjacency
+    const int U = 0, V = 1;
+    const int HI = 1, LO = -1;
+
+    // Index of face on the left, axis to read from, and sign (in pixel coords)
+    //                              +X               -X               +Y              -Y              +Z               -Z 
+    const int left[]      = {CubeFace::NEG_Z, CubeFace::POS_Z, CubeFace::POS_X, CubeFace::POS_X, CubeFace::POS_X, CubeFace::NEG_X};
+    const int leftAxis[]  = {        U,                U,               V,               V,            U,                U       };
+    const int leftSign[]  = {        HI,               HI,              LO,              HI,           HI,               HI      };
+
+    //                              +X               -X               +Y              -Y              +Z               -Z 
+    const int right[]     = {CubeFace::POS_Z, CubeFace::NEG_Z, CubeFace::NEG_X, CubeFace::NEG_X, CubeFace::NEG_X, CubeFace::POS_X};
+    const int rightAxis[] = {        U,                U,               V,               V,            U,                U       };
+    const int rightSign[] = {        LO,               LO,              LO,              HI,           LO,               LO      };
+
+    //                              +X               -X               +Y              -Y              +Z               -Z 
+    const int top[]       = {CubeFace::POS_Y, CubeFace::POS_Y, CubeFace::NEG_Z, CubeFace::POS_Z, CubeFace::POS_Y, CubeFace::POS_Y};
+    const int topAxis[]   = {        U,                U,               V,               V,            V,                V       };
+    const int topSign[]   = {        LO,               HI,              LO,              HI,           LO,               HI      };
+
+    //                              +X               -X               +Y              -Y              +Z               -Z 
+    const int bottom[]    = {CubeFace::NEG_Y, CubeFace::NEG_Y, CubeFace::POS_Z, CubeFace::NEG_Z, CubeFace::NEG_Y, CubeFace::NEG_Y};
+    const int bottomAxis[]= {        U,                U,               V,               V,            V,                V       };
+    const int bottomSign[]= {        LO,               HI,              LO,              HI,           LO,               HI      };
+
     // Construct the source images
     for (int f = 0; f < 6; ++f) {
 
-        const Image& src = *face[f].get();
         Image& dst = m_faceArray[f];
-
         dst.setSize(m_iSize + 2, m_iSize + 2, format);
         // Copy the interior
+        {
+            const Image& src = *face[f].get();
+            Thread::runConcurrently(Point2int32(0, 0), Point2int32(m_iSize, m_iSize), [&](Point2int32 P) {
+                dst.set(P + Point2int32(1, 1), src.get<Color4>(P));
+            });
+        }
 
-        Thread::runConcurrently(Point2int32(0, 0), Point2int32(m_iSize, m_iSize), [&](Point2int32 P) {
-            dst.set(P + Point2int32(1, 1), src.get<Color4>(P));
-        });
+        // Copy left edge
+        {
+            const Image& src = *face[left[f]].get();
+            const int fixedAxis     = leftAxis[f];
+            const int iterationAxis = leftAxis[f];
+            const int sign          = leftSign[f];
+            Point2int32 P;
+            P[fixedAxis] = (sign == HI) ? m_iSize - 1 : 0;
+            for (int i = 0; i < m_iSize; ++i) {
+                P[iterationAxis] = i;
+                dst.set(m_iSize + 1, i + 1, src.get<Color4>(P));
+            }
+        }
+
+        // Copy right edge
+        {
+            const Image& src = *face[right[f]].get();
+            const int fixedAxis     = rightAxis[f];
+            const int iterationAxis = rightAxis[f];
+            const int sign          = rightSign[f];
+            Point2int32 P;
+            P[fixedAxis] = (sign == HI) ? m_iSize - 1 : 0;
+            for (int i = 0; i < m_iSize; ++i) {
+                P[iterationAxis] = i;
+                dst.set(0, i + 1, src.get<Color4>(P));
+            }
+        }
+
+        // Copy top edge
+        {
+            const Image& src  = *face[top[f]].get();
+            const int fixedAxis     = topAxis[f];
+            const int iterationAxis = topAxis[f];
+            const int sign          = topSign[f];
+            Point2int32 P;
+            P[fixedAxis] = (sign == HI) ? m_iSize - 1 : 0;
+            for (int i = 0; i < m_iSize; ++i) {
+                P[iterationAxis] = i;
+                dst.set(i + 1, 0, src.get<Color4>(P));
+            }
+        }
+
+        // Copy bottom edge
+        {
+            const Image& src  = *face[bottom[f]].get();
+            const int fixedAxis     = bottomAxis[f];
+            const int iterationAxis = bottomAxis[f];
+            const int sign          = bottomSign[f];
+            Point2int32 P;
+            P[fixedAxis] = (sign == HI) ? m_iSize - 1 : 0;
+            for (int i = 0; i < m_iSize; ++i) {
+                P[iterationAxis] = i;
+                dst.set(i + 1, m_iSize + 1, src.get<Color4>(P));
+            }
+        }
     }
+
+    // TODO: copy corners
 
     m_fSize = float(m_iSize);
 
-    // TODO: copy edges and corners
 }
 
 
@@ -69,8 +151,10 @@ Vector2 CubeMap::pixelCoord(const Vector3& vec, CubeFace& face) const {
     const Vector3::Axis uAxis = Vector3::Axis((faceAxis + 1) % 3);
     const Vector3::Axis vAxis = Vector3::Axis((faceAxis + 2) % 3);
 
+    // Texture coordinate, where (0, 0) is the upper left of the image
     Vector2 texCoord = 0.5f * Vector2(vec[uAxis], vec[vAxis]) / fabsf(vec[faceAxis]) + Vector2(0.5f, 0.5f);
 
+    // Correct for OpenGL cube map rules
     switch (face) {
     case CubeFace::POS_X:
         texCoord = Vector2(1.0f - texCoord.y, 1.0f - texCoord.x);
