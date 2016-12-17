4b4144f9714b2270733ca109d5869b55a19ed864
morgan <morgan@9296df4a-2249-4d1b-8fb0-1ab621adc933>
2016-12-17 17:55:06 +0000

    Add prefix tree data structure

---
+++ G3D10/G3D.lib/source/PrefixTree.cpp
@@ -0,0 +1,102 @@
+#include "G3D/PrefixTree.h"
+
+namespace G3D {
+    
+PrefixTree::PrefixTree(const String& s) : m_value(s) {}
+PrefixTree::~PrefixTree() {}
+
+bool PrefixTree::canHaveChildren() {
+    return m_children.size() > 0 || m_value == "";
+}
+
+const shared_ptr<PrefixTree> PrefixTree::childNodeWithPrefix(const String& s) {
+   for (shared_ptr<PrefixTree>& child : m_children) {
+        if (child->value() == s && child->canHaveChildren()) {
+            return child;
+        }
+    }
+    return nullptr;
+}
+
+void PrefixTree::insert(const String& s) {
+    const Array<String> components = compactSplit(s, ' '); // TODO delimiter
+
+    // Iterate to *parent* of leaf of existing prefix tree
+    // The leaves store the original representation of the element, preserving
+    // whitespace, so we should never alter them and instead stop at the parent 
+    shared_ptr<PrefixTree> finger = shared_from_this();
+    int i = 0;
+    while (i < components.size()) {
+        // Check if any of the finger's children contain the next component
+        const shared_ptr<PrefixTree>& next = finger->childNodeWithPrefix(components[i]);
+        
+        // If not, then we are at the insertion point
+        if (isNull(next)) {
+            break;
+        }
+
+        // If so, continue traversing the prefix tree
+        finger = next;
+        ++i;
+    }
+
+    // Add nodes as necessary, starting at the insertion point
+    while (i < components.size()) {
+        const String& component(components[i]);
+
+        shared_ptr<PrefixTree> next = PrefixTree::create(component);
+        finger->m_children.append(next);
+        finger = next;
+
+        ++i;
+    }
+
+    // Add leaf node with precise value of string
+    finger->m_children.append(std::make_shared<PrefixTree>(s));\
+    ++m_size;
+}
+
+bool PrefixTree::contains(const String& s) {
+    const Array<String> components = compactSplit(s, ' '); // TODO delimiter
+
+    // Iterate to *parent* of leaf of existing prefix tree
+    // The leaves store the original representation of the element, preserving
+    // whitespace, so we should never alter them and instead stop at the parent 
+    shared_ptr<PrefixTree>& finger = shared_from_this();
+    for (const String& component : components) {
+        // Check if any of the finger's children contain the next component
+        const shared_ptr<PrefixTree>& next = finger->childNodeWithPrefix(component);
+
+        if (!next) {
+            return false;
+        }
+        finger = next;
+    }
+    
+    // Check that the leaves that extend 
+    for (const shared_ptr<PrefixTree>& node : finger->m_children) {
+        if (node->isLeaf() && node->value() == s) {
+            return true;
+        } 
+    }
+    return false;
+}
+
+String PrefixTree::getPathToBranch(shared_ptr<PrefixTree>& branchPoint) {
+    shared_ptr<PrefixTree>& finger(shared_from_this());
+                
+    Array<String> pathParts;
+    while (finger->m_children.size() == 1) {
+        pathParts.append(finger->value());
+        finger = finger->m_children[0];
+    }
+    // Omit leaf nodes from the string
+    if (finger->m_children.size() > 0) {
+        pathParts.append(finger->value());
+    }
+    
+    branchPoint = finger;
+    return compactJoin(pathParts, ' '); // TODO maybe do this in the caller
+}
+
+}
