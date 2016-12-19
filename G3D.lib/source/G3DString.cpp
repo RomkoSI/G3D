337ffe3b42a81101fdb2260ac24e1071f467520d
asm4 <asm4@9296df4a-2249-4d1b-8fb0-1ab621adc933>
2016-12-19 02:34:13 +0000

    detection of const seg strings on OS X

--- G3D.lib/source/G3DString.cpp
+++ G3D.lib/source/G3DString.cpp
@@ -7,13 +7,14 @@ static const char* testStr = "__HackString.h__";
 static const uintptr_t PROBED_CONST_SEG_ADDR = uintptr_t(testStr);
 
 #ifdef G3D_WINDOWS
-#define abs _abs64
+ #define abs _abs64
 #endif
 
 bool inConstSegment(const char* c) {
-#   if defined(G3D_64BIT) && _MSC_VER
+#   if defined(G3D_64BIT) //&& _MSC_VER commented out because it should work on OSX now. zandermajercik 2016-11-05
         // MSVC assigns the const_seg to very high addresses that are grouped together
         //return uintptr_t(c) >= MIN_CONST_SEG_ADDR;
+
         return (abs(int64(uintptr_t(c) - PROBED_CONST_SEG_ADDR)) < int64(50000000));
 #   else
         return false;
